a889a9f76a850dd5306ab3fa2117c60e0443f32b
Faizaan Alam <faizaanalam.connect@gmail.com>
2026-06-29 03:45:58 +0530

    notes app basic done

---
+++ 04backend/server.js
@@ -0,0 +1,7 @@
+
+const app = require('./src/app');//we are importing the app instance from the app.js file so that we can use it to create a server and listen for incoming requests.
+
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
